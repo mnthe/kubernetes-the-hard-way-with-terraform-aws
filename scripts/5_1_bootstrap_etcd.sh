6e6cf952e60c8d989cbc3496445c043c4d32badd
JungHun Kim <mnthe@pubg.com>
2020-04-21 15:52:40 +0900

    Bootstrap etcd in parallel

--- scripts/5_1_bootstrap_etcd.sh
+++ scripts/5_1_bootstrap_etcd.sh
@@ -1,7 +1,7 @@
 TERRAFORM_OUTPUT=$(terraform output --json)
 # controller-0
-ssh -i ssh/ssh.pem ubuntu@$(echo $TERRAFORM_OUTPUT | jq -r ".controller_public_ips.value[0]") 'bash -s' < ./locals/5_1_bootstrap_etcd.sh
+ssh -i ssh/ssh.pem ubuntu@$(echo $TERRAFORM_OUTPUT | jq -r ".controller_public_ips.value[0]") 'bash -s' < ./locals/5_1_bootstrap_etcd.sh &
 # controller-1
-ssh -i ssh/ssh.pem ubuntu@$(echo $TERRAFORM_OUTPUT | jq -r ".controller_public_ips.value[1]") 'bash -s' < ./locals/5_1_bootstrap_etcd.sh
+ssh -i ssh/ssh.pem ubuntu@$(echo $TERRAFORM_OUTPUT | jq -r ".controller_public_ips.value[1]") 'bash -s' < ./locals/5_1_bootstrap_etcd.sh &
 # controller-2
 ssh -i ssh/ssh.pem ubuntu@$(echo $TERRAFORM_OUTPUT | jq -r ".controller_public_ips.value[2]") 'bash -s' < ./locals/5_1_bootstrap_etcd.sh
